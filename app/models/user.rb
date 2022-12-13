89ad3013c9e16f7c6393a42564dbaa7c46a7f809
angeluray <aeucampos@gmail.com>
2022-12-13 15:42:35 -0400

    Add cancan conditions(first version)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
-  load_and_authorize_resource
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :foods, foreign_key: :user_id, dependent: :destroy
   has_many :recipes, foreign_key: :user_id, dependent: :destroy
+  validates :name, presence: true
 
   Roles = [ :admin , :default ]
 
@@ -13,5 +13,4 @@ class User < ApplicationRecord
     self.role == requested_role.to_s
   end
 
-  validates :name, presence: true
 end
