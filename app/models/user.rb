35752416f27eaafe08520e75487b5ec71a3effc7
angeluray <aeucampos@gmail.com>
2022-12-13 14:39:38 -0400

    Set up cancan(Basic configuration)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,17 @@
 class User < ApplicationRecord
+  load_and_authorize_resource
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :foods, foreign_key: :user_id, dependent: :destroy
   has_many :recipes, foreign_key: :user_id, dependent: :destroy
+
+  Roles = [ :admin , :default ]
+
+  def is?( requested_role )
+    self.role == requested_role.to_s
+  end
 
   validates :name, presence: true
 end
