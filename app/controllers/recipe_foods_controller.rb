94178b870b50958f3ba03db52376d9ed07a0a1d0
angeluray <aeucampos@gmail.com>
2022-12-13 14:40:01 -0400

    Add recipe food functionality

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -1,5 +1,40 @@
 class RecipeFoodsController < ApplicationController
+    before_action :authenticate_user!
+
   def index
     @foods = Food.all
+  end
+
+  def new
+    @recipe = Recipe.find(params[:recipe_id])
+    @recipe_food = @recipe.recipe_foods.new
+  end
+
+  def create
+    @recipe = Recipe.find(params[:recipe_id])
+    @recipe_food = @recipe.recipe_foods.create(recipe_foods_params)
+    if @recipe_food.save
+      flash[:notice] = 'Food created sucessfully.'
+      redirect_to @recipe
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @recipe_food = RecipeFood.find(params[:id])
+  end
+
+  def destroy
+    @recipe_food = RecipeFood.find(params[:id])
+    @recipe_food.destroy
+    flash[:success] = 'Recipe Food deleted successfully.'
+    redirect_to recipe_path(@recipe_food.recipe_id)
+  end
+
+  private
+
+  def recipe_foods_params
+    params.require(:recipe_food).permit(:quantity, :food_id)
   end
 end
