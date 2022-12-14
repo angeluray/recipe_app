8bab851e54d0df7eea1143eb4cdf2c0f9b92c70c
Federico Luna <fluna1973@yahoo.com>
2022-12-14 17:58:09 -0600

    fix linters

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,34 +4,12 @@ class Ability
   def initialize(user)
     return unless user.present?
 
-    can :read, Recipe, user: user
-    can :manage, Recipe, user: user
-    can :manage, Food, user: user
-    can :manage, RecipeFood, user: user
+    can :read, Recipe, user
+    can :manage, Recipe, user
+    can :manage, Food, user
+    can :manage, RecipeFood, user
     return unless user.role == 'admin'
 
     can :manage, :all
   end
 end
-
-# class Ability
-#   include CanCan::Ability
-
-#   def initialize(user)
-#     user ||= User.new
-#     # Define User abilities
-#     if user.is? :admin
-#       can :manage, Recipe do |recipe|
-#         recipe.user_id == user.id
-#       end
-#       can :read, Recipe, &:public
-#       can :create, Food
-#       can :destroy, Food do |food|
-#         food.user_id == user.id
-#       end
-#     else
-#       can :read, Food
-#       can :read, Recipe, &:public
-#     end
-#   end
-# end
