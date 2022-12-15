444fdb791289d241b259ccf0b149b7e8229861f3
angeluray <aeucampos@gmail.com>
2022-12-15 01:46:34 -0400

    Add model tests

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -9,12 +9,17 @@ RSpec.describe Recipe, type: :model do
                             description: 'Best meet ever', public: true)
   end
 
-  it 'name is required' do
+  it 'is created succesfully' do
     expect(@recipe).to be_valid
   end
 
   it 'Name is required' do
     @recipe.name = nil
+    expect(@recipe).to_not be_valid
+  end
+
+  it 'doesnt create the recipe without a user id/user' do
+    @recipe.user = nil
     expect(@recipe).to_not be_valid
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,9 +6,23 @@ RSpec.describe User, type: :model do
     @user.save
   end
 
+  it "is created succesfully" do
+    expect(@user).to be_valid
+  end
+
   it 'name is required' do
     expect(@user).to be_valid
     @user.name = nil
+    expect(@user).not_to be_valid
+  end
+
+  it 'is not valid without email' do
+    @user.email = nil
+    expect(@user).not_to be_valid
+  end
+
+  it 'is not valid without password' do
+    @user.password = ""
     expect(@user).not_to be_valid
   end
 end
