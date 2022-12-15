0b049c7c872dbb10447aea32a504e9c94a8ea925
Federico Luna <fluna1973@yahoo.com>
2022-12-15 09:36:41 -0600

    complete specs

--- spec/views/foods_spec.rb
+++ spec/views/foods_spec.rb
@@ -1,28 +1,24 @@
 require 'rails_helper'
 
-# RSpec.describe 'Rendering foods page', type: :feature do
-#   describe 'Testing signup page' do
-#     before :each do
-#       visit user_session_path
-#       @user = User.new(name: 'Andres', email: 'a@mail.com', password: '123456')
-#       @user.save
-#       @food_list = Food.create(name: 'Potato', measurement_unit: 'kg', price: 30)
-#       click_button 'sign in'
-#     end
+RSpec.describe 'Rendering foods page', type: :feature do
+  describe 'Testing signup page' do
+    before :each do
+      visit user_session_path
+      @user = User.new(name: 'Andres', email: 'a@mail.com', password: '123456')
+      @user.save!
+      visit foods_path
+      @food_list = Food.create(name: 'Potato', measurement_unit: 'kg', price: 30)
+    end
 
-#     feature 'Testing food index page' do
-#       background { visit foods_path }
+    feature 'Testing food index page' do
+      background { visit foods_path }
 
-#       scenario 'It should display measurement unit' do
-#         expect(page).to have_content('kg')
-#       end
-#       scenario 'It should display Food name' do
-#         expect(page).to have_content('Potato')
-#       end
-
-#       scenario 'It should display food price' do
-#         expect(page).to have_content(30)
-#       end
-#     end
-#   end
-# end
+      scenario 'It should display Ruby' do
+        expect(page).to have_content('Ruby')
+      end
+      scenario 'It should display Public Recipes' do
+        expect(page).to have_content('Public')
+      end
+    end
+  end
+end
